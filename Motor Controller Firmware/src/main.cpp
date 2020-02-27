5298e23cc543ede067afea801a95337dddfd5007
Alex Liu <contactalexliu@users.noreply.github.com>
2020-02-26 21:21:54 -0800

    Added configuration options

--- Motor Controller Firmware/src/main.cpp
+++ Motor Controller Firmware/src/main.cpp
@@ -8,6 +8,12 @@
 
 #include <Arduino.h>
 
+/******************** BEGIN Configuration ********************/
+#define TELEMETRY true
+#define OVERCURRENT_PROTECTION true
+#define SOLAR_MPPT false
+/******************** END Configuration **********************/
+
 /******************** BEGIN Pin Definitions ******************/
 #define dir1 3
 #define dir2 11
@@ -30,35 +36,36 @@ unsigned long previousMillis = 0;               // (ms)
 unsigned long currentMillis;                    // (ms)
 const long interval = rampTime / pwmResolution; // (ms)
 
-int current;     // (?)
-int duty;        // (%)
+int current;      // (?)
+int duty = 0;     // (%)
+int lastDuty = 0; // (%)
 
-bool buttonPressed = false;
-bool lastButtonState = false;
-bool doneRamping = false;
+bool buttonPressed;
 /******************** END Global Variables *******************/
 
 /******************** BEGIN Setup ****************************/
 void setup()
 {
-  Serial.begin(9600);
+  #if TELEMETRY
+    Serial.begin(9600);
+  #endif
   /********** Configure Pins **********/
   // (0.67 volts is 10 amps, a good limit per driver)
   // (revised, analog 0, 1 volt delta is 20 amps delta with a 1k resistor, current ratio of 20,000)
-  pinMode(dir1, OUTPUT);          // IN_1
-  // pinMode(dir2, OUTPUT);       // IN_2 (hardwired from Pin 3)
-  pinMode(enable1, OUTPUT);       // INH_1
-  pinMode(enable2, OUTPUT);       // INH_2
-  pinMode(button, INPUT_PULLUP);  // Pin 0
-  pinMode(isense1, INPUT);        // IS_1
-  pinMode(isense2, INPUT);        // IS_2
-  pinMode(vsense, INPUT);         // Vbat
+  pinMode(dir1, OUTPUT);         // IN_1
+  pinMode(dir2, OUTPUT);         // IN_2
+  pinMode(enable1, OUTPUT);      // INH_1
+  pinMode(enable2, OUTPUT);      // INH_2
+  pinMode(button, INPUT_PULLUP); // Pin 0
+  pinMode(isense1, INPUT);       // IS_1
+  pinMode(isense2, INPUT);       // IS_2
+  pinMode(vsense, INPUT);        // Vbat
 
   /********** Set Pins ****************/
-  digitalWrite(dir1, LOW);      // Make sure IN_1 starts OFF
-  // digitalWrite(dir2, LOW);   // Make sure IN_2 starts OFF (hardwired to mirror IN_1)
-  digitalWrite(enable1, HIGH);  // Enable Output 1
-  digitalWrite(enable2, LOW);   // Disable Output 2
+  digitalWrite(dir1, LOW);     // Make sure IN_1 starts OFF
+  digitalWrite(dir2, LOW);     // Make sure IN_2 starts OFF
+  digitalWrite(enable1, HIGH); // Enable Output 1
+  digitalWrite(enable2, LOW);  // Disable Output 2
 }
 /******************** END Setup ******************************/
 
@@ -86,6 +93,20 @@ void setPWM()
 /******************** BEGIN Main Loop ************************/
 void loop()
 {
+  if (duty < 1)
+  {
+    duty = 0;
+  }
+  else if (duty > 100)
+  {
+    duty = 100;
+  }
+
+  if (lastDuty != duty)
+  {
+    setPWM();
+  }
+
   if (digitalRead(button) == HIGH)
   {
     buttonPressed = false;
@@ -95,51 +116,42 @@ void loop()
     buttonPressed = true;
   }
 
-  if (buttonPressed != lastButtonState)
-  {
-    doneRamping = false;
-  }
-
-  lastButtonState = buttonPressed;
-
   // Check whether current and duty should be updated.
-  if (!doneRamping)
+  currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
   {
-    currentMillis = millis();
-    if (currentMillis - previousMillis >= interval)
+    previousMillis = currentMillis;
+
+    if (buttonPressed && duty < 100)
     {
-      previousMillis = currentMillis;
-
-      if (buttonPressed && duty < 100)
-      {
-        duty++;
-      }
-      else if (!buttonPressed && duty > 0)
-      {
-        duty--;
-      }
-      else
-      {
-        doneRamping = true;
-      }
-      setPWM();
-
-      current = analogRead(isense1);
-      Serial.println(current);
+      duty++;
     }
-  }
+    else if (!buttonPressed && duty > 0)
+    {
+      duty--;
+    }
+    setPWM();
 
-  /********** ?????????? **************/
-  if (current > 200)
-  {
-    duty -= 10;
+    current = analogRead(isense1);
+    #if TELEMETRY
+      Serial.println(current);
+    #endif
   }
+  lastDuty = duty;
 
-  /********** Solar MPPT **************/
-  // Keep input voltage above 30 volts (30/11 * 1024/5 = 559).
-  if (analogRead(vsense) < 559)
-  {
-    duty -= 10;
-  }
+  #if OVERCURRENT_PROTECTION
+    if (current > 200)
+    {
+      duty -= 1;
+    }
+  #endif
+
+  #if SOLAR_MPPT
+    // Keep input voltage above 30 volts (30/11 * 1024/5 = 559).
+    if (analogRead(vsense) < 559)
+    {
+      duty -= 1;
+    }
+  #endif
 }
 /******************** END Main Loop **************************/
