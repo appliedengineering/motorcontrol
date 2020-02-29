7893766c0ab60acc825e594a581ac43da1edea1d
Alex Liu <contactalexliu@users.noreply.github.com>
2020-02-28 19:45:22 -0800

    Add option to parallel outputs

--- Motor Controller Firmware/src/main.cpp
+++ Motor Controller Firmware/src/main.cpp
@@ -7,12 +7,18 @@
  */
 
 #include <Arduino.h>
+#include <avdweb_AnalogReadFast.h>
+#include <digitalWriteFast.h>
 
 /******************** BEGIN Configuration ********************/
 #define MINIMUM_DUTY_DETECTION false
-#define TELEMETRY true
 #define OVERCURRENT_PROTECTION false
-#define SOLAR_MPPT false
+#define PARALLEL_OUTPUTS true
+#define TELEMETRY true
+
+// Enable these features when running on solar.
+#define OVERVOLTAGE_PREVENTION false
+#define UNDERVOLTAGE_PREVENTION false
 /******************** END Configuration **********************/
 
 /******************** BEGIN Pin Definitions ******************/
@@ -27,24 +33,31 @@
 /******************** END Pin Definitions ********************/
 
 /******************** BEGIN Global Variables *****************/
-#define pwmFreq 20000 // (Hz)
-#define pwmResolution ((F_CPU / 8) / pwmFreq)
+#if PARALLEL_OUTPUTS
+  unsigned int duty = 10;
+  int timeOn, timeOff, current, zeroCurrent, buttonPressed;
+  int rampSpeed = 10; // 4 * seconds to go from off to full or vice versa
+  int period = 100;
+#else
+  #define pwmFreq 20000 // (Hz)
+  #define pwmResolution ((F_CPU / 8) / pwmFreq)
 
-// The motor will take 5 seconds to ramp up or ramp down.
-#define rampTime 5 * 1000 // (ms)
+  // The motor will take 5 seconds to ramp up or ramp down.
+  #define rampTime 5 * 1000 // (ms)
 
-unsigned long previousMillis = 0;               // (ms)
-unsigned long currentMillis;                    // (ms)
-const long interval = rampTime / pwmResolution; // (ms)
+  unsigned long previousMillis = 0;               // (ms)
+  unsigned long currentMillis;                    // (ms)
+  const long interval = rampTime / pwmResolution; // (ms)
 
-int current;        // (?)
-#if MINIMUM_DUTY_DETECTION
-  int zeroCurrent;  // (?)
+  int current;        // (?)
+  #if MINIMUM_DUTY_DETECTION
+    int zeroCurrent;  // (?)
+  #endif
+  int duty = 0;       // (%)
+  int lastDuty = 0;   // (%)
+
+  bool buttonPressed;
 #endif
-int duty = 0;       // (%)
-int lastDuty = 0;   // (%)
-
-bool buttonPressed;
 /******************** END Global Variables *******************/
 
 /******************** BEGIN Setup ****************************/
@@ -76,103 +89,204 @@ void setup()
     // Initialize ADC circuitry and discard first dummy sample
     (void)analogRead(isense1);
     // Can only measure current when HIGH, so we must pulse IN_1
-    digitalWrite(dir1, HIGH);
-    zeroCurrent = analogRead(isense1);
-    digitalWrite(dir1, LOW);
+    digitalWriteFast(dir1, HIGH);
+    zeroCurrent = analogReadFast(isense1);
+    digitalWriteFast(dir1, LOW);
   #endif
 }
 /******************** END Setup ******************************/
 
-/******************** BEGIN PWM Function *********************/
-void setPWM()
-{
-  if (duty == 0)
+#if PARALLEL_OUTPUTS
+  /******************** BEGIN Main Loop ************************/
+  void loop()
   {
-    // Turn off PWM
-    TCCR2B = _BV(WGM22);
-  }
-  else if (lastDuty != 0)
-  {
-    OCR2B = duty - 1;
-  }
-  else
-  {
-    // Set up 20kHz Fast PWM with OCR2A as TOP and Prescaler Divide Clock by 8 on Timer 2
-    // (http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf, P.130)
-    TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20) | _BV(WGM21);
-    TCCR2B = _BV(WGM22) | _BV(CS21);
-    OCR2A = pwmResolution - 1;
-    // duty = (OCR2B + 1) / (OCR2A + 1)
-    // OCR2B = ((OCR2A + 1) * duty) - 1
-    OCR2B = duty - 1;
-  }
-}
-/******************** END PWM Function ***********************/
-
-/******************** BEGIN Main Loop ************************/
-void loop()
-{
-  if (duty < 0)
-  {
-    duty = 0;
-  }
-  else if (duty > 100)
-  {
-    duty = 100;
-  }
-
-  if (duty != lastDuty)
-  {
-    setPWM();
-    lastDuty = duty;
-  }
-
-  if (digitalRead(button) == HIGH)
-  {
-    buttonPressed = false;
-  }
-  else
-  {
-    buttonPressed = true;
-  }
-
-  // Check whether current and duty should be updated.
-  currentMillis = millis();
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
-
-    if (buttonPressed && duty < 100)
+    if (duty < 50) duty = 50;     // If duty cycle < 1%, turn it off.
+    if (duty > 4900) duty = 4900; // Don't go above 99%.
+    // timeOn = (duty / 50) - 1;  // (µS) Assume min. on time is 1 µS, which is delayMicroseconds(0).
+    // timeOff = 100 - d2 - 1;    // (µS) Assume min. off time is 1 µS.
+    
+    timeOff = 500 - (duty / 10);  // Range from 500 µS down to almost zero.  Means we start at 2 kHz and frequency goes up as we ramp the motor up.
+    timeOn = 50 - timeOff;        // Constant 20 kHz period plus switching time if timeOff < 100 (if duty > 4500).
+    if (timeOn < 1) timeOn = 1;   // Min. on time unless duty > 4500
+    // So frequency only hits 20 kHz at the end of the scale and we get to 50%, our expected operating point at 4750.
+    
+    if (duty > 51)
     {
-      duty++;
-    }
-    else if (!buttonPressed && duty > 0)
-    {
-      duty--;
-    }
+      for (int i = 0; i < rampSpeed; i++) // Loop number of times = spin up or down time in seconds * 4.
+      {
+        // Briefly only 1 driver is working, but we can be sure dir1 and
+        // dir2 are never driving in opposite directions at the same time
 
-    current = analogRead(isense1);
-    #if MINIMUM_DUTY_DETECTION
-      current -= zeroCurrent;
-    #endif
+        // digitalWrite(enable2, LOW);    // INH_2 OFF, IN_2 HIGH (but INH_2 OFF), IN_1 HIGH, then INH_2 ON again
+        // digitalWrite(dir2, HIGH);
+        PORTB = 0b011000;                 // Simultaneously set INH_2 (13) OFF, INH_1 (12) ON, and IN_2 (11) HIGH
+        // digitalWrite(dir1, HIGH);
+        PORTD ^= 0b00001000;              // Toggle IN_1 (3) HIGH
+        // digitalWrite(enable2, HIGH);
+        PORTB ^= 0b100000;                // Toggle INH_2 (13) ON again
+        delayMicroseconds(timeOn);
+        // digitalWrite(enable2, LOW);    // Begin reversing the above
+        PORTB ^= 0b100000;                // Toggle INH_2 (13) OFF
+        // digitalWrite(dir1, LOW);
+        PORTD ^= 0b00001000;              // Toggle IN_1 (3) LOW
+        // digitalWrite(dir2, LOW);
+        // digitalWrite(enable2, HIGH);   // Trimmed to spend the least amount of time high possible
+        PORTB = 0b110000;                 // Simultaneously set INH_2 (13) ON, INH_1 (12) ON, and IN_2 (11) LOW
+        delayMicroseconds(timeOff);
+      }
+    }
+    else
+    {
+      delayMicroseconds(rampSpeed * 50);  // When in off state, we don't monkey with the IO.
+    }
+    
+    // Everything below only takes place every millisecond 10 pulses with the drivers LOW
+    // ADC reads 1023 for a 5 V input so raw = volts * 1024/5
+    // Current inputs are about .07 V per A according to datasheet, but we should test this.
+    
+    delayMicroseconds(20);  // To do a current reading we need both outputs HIGH
+    PORTB = 0b011000;       // Simultaneously set INH_2 (13) OFF, INH_1 (12) ON, and IN_2 (11) HIGH
+    PORTD ^= 0b00001000;    // Toggle IN_1 (3) HIGH
+    PORTB ^= 0b100000;      // Toggle INH_2 (13) ON again
+    delayMicroseconds(1);
+    current = analogReadFast(0);
+    PORTB ^= 0b100000;      // Toggle INH_2 (13) OFF
+    PORTD ^= 0b00001000;    // Toggle IN_1 (3) LOW
+    PORTB = 0b110000;       // Simultaneously set INH_2 (13) ON, INH_1 (12) ON, and IN_2 (11) LOW
+    delayMicroseconds(10);
+    current -= zeroCurrent;
     #if TELEMETRY
       Serial.println(current);
     #endif
-
+    
+    if (digitalRead(0) == HIGH) buttonPressed = 0; else buttonPressed = 1;
+    if (buttonPressed == 1) duty++; else duty--;
+    
     #if OVERCURRENT_PROTECTION
-      if (current > 200)
-      {
-        duty -= 2;
-      }
+      // 1 V delta is 20 A per driver -> 40 A (1 V * (1024 ADC values / 5 V) ≈ 200).
+      // I suggest 30 A per driver (300).
+      // Battery current = this current * duty.
+      if (current > 300) duty = duty - 10;
+    #endif
+    
+    #if MINIMUM_DUTY_DETECTION
+      // Don't reduce duty cycle unless current is positive.
+      if (current < 0) duty = duty + 1;
     #endif
 
-    #if SOLAR_MPPT
-      // Keep input voltage above 30 volts (30/11 * 1024/5 = 559).
-      if (analogRead(vsense) < 559)
-      {
-        duty -= 2;
-      }
+    #if UNDERVOLTAGE_PREVENTION
+      if (analogRead(vsense) < 558) duty = duty - 10; // Keep input voltage above 30 volts (30/11 * 1024/5 = 559) to get more power out on solar.
+    #endif
+    #if OVERVOLTAGE_PREVENTION
+      if (analogRead(vsense) > 744) duty = duty + 3;  // Keep input voltage below 40 volts (40/11 * 1024/5 = 745) to protect against regen on solar.
     #endif
   }
-}
-/******************** END Main Loop **************************/
+  /******************** END Main Loop **************************/
+#else
+  /******************** BEGIN Fast PWM Function ****************/
+  void setPWM()
+  {
+    if (duty == 0)
+    {
+      // Turn off PWM
+      TCCR2B = _BV(WGM22);
+    }
+    else if (lastDuty != 0)
+    {
+      OCR2B = duty - 1;
+    }
+    else
+    {
+      // Set up 20kHz Fast PWM with OCR2A as TOP and Prescaler Divide Clock by 8 on Timer 2
+      // (http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf, P.130)
+      TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20) | _BV(WGM21);
+      TCCR2B = _BV(WGM22) | _BV(CS21);
+      OCR2A = pwmResolution - 1;
+      // duty = (OCR2B + 1) / (OCR2A + 1)
+      // OCR2B = ((OCR2A + 1) * duty) - 1
+      OCR2B = duty - 1;
+    }
+  }
+  /******************** END Fast PWM Function ******************/
+
+  /******************** BEGIN Main Loop ************************/
+  void loop()
+  {
+    if (duty < 0)
+    {
+      duty = 0;
+    }
+    else if (duty > 100)
+    {
+      duty = 100;
+    }
+
+    if (duty != lastDuty)
+    {
+      setPWM();
+      lastDuty = duty;
+    }
+
+    if (digitalRead(button) == HIGH)
+    {
+      buttonPressed = false;
+    }
+    else
+    {
+      buttonPressed = true;
+    }
+
+    // Check whether current and duty should be updated.
+    currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+      previousMillis = currentMillis;
+
+      if (buttonPressed && duty < 100)
+      {
+        duty++;
+      }
+      else if (!buttonPressed && duty > 0)
+      {
+        duty--;
+      }
+
+      current = analogRead(isense1);
+      #if MINIMUM_DUTY_DETECTION
+        current -= zeroCurrent;
+        if (current < 0)
+        {
+          duty++;
+        }
+      #endif
+      #if TELEMETRY
+        Serial.println(current);
+      #endif
+
+      #if OVERCURRENT_PROTECTION
+        if (current > 300)
+        {
+          duty -= 2;
+        }
+      #endif
+
+      #if UNDERVOLTAGE_PREVENTION
+        // Keep input voltage above 30 volts (30/11 * 1024/5 = 559).
+        // Gets more power out on solar.
+        if (analogRead(vsense) < 559)
+        {
+          duty -= 2;
+        }
+      #endif
+      #if OVERVOLTAGE_PREVENTION
+        // Keep input voltage below 40 volts (40/11 * 1024/5 = 745).
+        // Protects against regen on solar.
+        if (analogRead(vsense) > 744)
+        {
+          duty += 2;
+        }
+      #endif
+    }
+  }
+  /******************** END Main Loop **************************/
+#endif
