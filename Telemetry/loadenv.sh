027d4c2004c3d341aab9c3536eaaec88031c768c
Alex Liu <contactalexliu@users.noreply.github.com>
2020-06-25 23:06:08 -0700

    Fix virtualenv script

--- Telemetry/loadenv.sh
+++ Telemetry/loadenv.sh
@@ -5,7 +5,7 @@
 ###################################
 
 # Check if Python 3 is installed
-if [ ! type "python" > /dev/null ]; then
+if ! type "python" > /dev/null; then
     echo "Couldn't find python. Install python 3 first."
 elif [ ! $(python -c"import sys; print(sys.version_info.major)") -eq 3 ]; then
 	echo "The command 'python' is not running python 3 (probably python 2). Please install python 3 to run as 'python' instead."
@@ -14,12 +14,12 @@ else
 fi
 
 # Check if pip is installed
-if [ ! type "pip" > /dev/null ]; then
+if ! type "pip" > /dev/null; then
     echo "Couldn't find pip. Install pip for python 3 first."
 fi
 
 # Check if virtualenv is installed
-if [ ! type "virtualenv" > /dev/null ]; then
+if ! type "virtualenv" > /dev/null; then
     pip install virtualenv
 fi
 
