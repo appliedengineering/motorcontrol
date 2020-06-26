f08e1a4d206344b4499d887bbc1b8a054677d92d
Alex Liu <contactalexliu@users.noreply.github.com>
2020-06-25 23:03:08 -0700

    Better virtualenv script

--- Telemetry/loadenv.sh
+++ Telemetry/loadenv.sh
@@ -4,6 +4,25 @@
 # Run using "source ./loadenv.sh" #
 ###################################
 
+# Check if Python 3 is installed
+if [ ! type "python" > /dev/null ]; then
+    echo "Couldn't find python. Install python 3 first."
+elif [ ! $(python -c"import sys; print(sys.version_info.major)") -eq 3 ]; then
+	echo "The command 'python' is not running python 3 (probably python 2). Please install python 3 to run as 'python' instead."
+else
+    echo "Found python 3."
+fi
+
+# Check if pip is installed
+if [ ! type "pip" > /dev/null ]; then
+    echo "Couldn't find pip. Install pip for python 3 first."
+fi
+
+# Check if virtualenv is installed
+if [ ! type "virtualenv" > /dev/null ]; then
+    pip install virtualenv
+fi
+
 # Create virtualenv if does not exist 
 if [ ! -d ".env" ]; then
     virtualenv .env
