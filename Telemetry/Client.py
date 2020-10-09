d14a639bb412b9776c37a6ea2d0bc93d3a283792
Restyled.io <commits@restyled.io>
2020-10-09 05:34:51 +0000

    Restyled by black

--- Telemetry/Client.py
+++ Telemetry/Client.py
@@ -7,15 +7,17 @@ if len(sys.argv) == 3:
     ip = sys.argv[1]
     port = int(sys.argv[2])
 else:
-    print("Run like : python3 client.py <arg1 server ip 192.168.1.102> <arg2 server port 4444 >")
+    print(
+        "Run like : python3 client.py <arg1 server ip 192.168.1.102> <arg2 server port 4444 >"
+    )
     exit(1)
 
 # Create a UDP socket
-#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to the port
-#server_address = (ip, port)
+# server_address = (ip, port)
 # s.bind(server_address)
-#print("Do Ctrl+c to exit the program !!")
+# print("Do Ctrl+c to exit the program !!")
 
 MCAST_GRP = ip
 MCAST_PORT = port
@@ -25,23 +27,24 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 if IS_ALL_GROUPS:
     # on this port, receives ALL multicast groups
-    sock.bind(('', MCAST_PORT))
+    sock.bind(("", MCAST_PORT))
 else:
     # on this port, listen ONLY to MCAST_GRP
     sock.bind((MCAST_GRP, MCAST_PORT))
 mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
-                mreq)  # Let's send data through UDP protocol
+sock.setsockopt(
+    socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq
+)  # Let's send data through UDP protocol
 
 while True:
     # send_data = input("Type some text to send =>")
     # s.sendto(send_data.encode('utf-8'), (ip, port))
     # print("\n\n 1. Client Sent : ", send_data, "\n\n")
     # print("recieve")
-    #binaryData, address = s.recvfrom(64)
+    # binaryData, address = s.recvfrom(64)
     # print(binaryData.decode('utf-8'))
-    #telemetryData = struct.unpack('<12shhhhfffff???', binaryData)
+    # telemetryData = struct.unpack('<12shhhhfffff???', binaryData)
     # print(telemetryData)
     print("data found")
     print(sock.recv(10240))
--- Telemetry/Server.py
+++ Telemetry/Server.py
@@ -11,7 +11,9 @@ if len(sys.argv) == 3:
     ip = sys.argv[1]
     port = int(sys.argv[2])
 else:
-    print("Run like : python3 server.py <arg1:server ip:this system IP 192.168.1.6> <arg2:server port:4444 >")
+    print(
+        "Run like : python3 server.py <arg1:server ip:this system IP 192.168.1.6> <arg2:server port:4444 >"
+    )
     exit(1)
 
 # Creates a UDP socket
@@ -20,37 +22,37 @@ print("Do Ctrl+C to exit the program.")
 
 
 def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        if platform.system() == 'Linux':
-            link = txfer.SerialTransfer('/dev/ttyACM0', 115200)
-        elif platform.system() == 'Darwin':
-            link = txfer.SerialTransfer('/dev/cu.usbmodem14101', 115200, False)
+        if platform.system() == "Linux":
+            link = txfer.SerialTransfer("/dev/ttyACM0", 115200)
+        elif platform.system() == "Darwin":
+            link = txfer.SerialTransfer("/dev/cu.usbmodem14101", 115200, False)
         else:
-            link = txfer.SerialTransfer('COM1', 115200)
+            link = txfer.SerialTransfer("COM1", 115200)
 
         if link.open():
             time.sleep(10)
 
             while True:
                 # while not link.available():
-                 #   if link.status < 0:
-                 #      if link.status == txfer.CRC_ERROR:
-                 #          print('ERROR: CRC_ERROR')
+                #   if link.status < 0:
+                #      if link.status == txfer.CRC_ERROR:
+                #          print('ERROR: CRC_ERROR')
                 #       elif link.status == txfer.PAYLOAD_ERROR:
                 #           print('ERROR: PAYLOAD_ERROR')
-               #        elif link.status == txfer.STOP_BYTE_ERROR:
+                #        elif link.status == txfer.STOP_BYTE_ERROR:
                 #           print('ERROR: STOP_BYTE_ERROR')
                 #       else:
                 #          print('ERROR: {}'.format(link.status))
 
-                #rawData = link.rxBuff[:link.bytesRead]
+                # rawData = link.rxBuff[:link.bytesRead]
                 # print(rawData)
-                #binaryData = bytearray(rawData)
-                #telemetryData = struct.unpack('<12shhhhfffff???', binaryData)
+                # binaryData = bytearray(rawData)
+                # telemetryData = struct.unpack('<12shhhhfffff???', binaryData)
                 # print(telemetryData)
 
                 # UDP MULTICAST BINARY DATA
@@ -61,13 +63,15 @@ if __name__ == '__main__':
                 MULTICAST_TTL = 2
 
                 sock = socket.socket(
-                    socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-                sock.setsockopt(socket.IPPROTO_IP,
-                                socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+                    socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+                )
+                sock.setsockopt(
+                    socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL
+                )
 
                 sock.sendto(b"test string", (ip, port))
-                #send_data = "TESTSTRING"
-                #s.sendto(send_data.encode('utf-8'), (ip, port))
+                # send_data = "TESTSTRING"
+                # s.sendto(send_data.encode('utf-8'), (ip, port))
                 # sends the data to client
                 # print("\n\n 1. Server sent ", len(send_data),"\n\n")                  # prints the data that was se
     except KeyboardInterrupt:
@@ -75,6 +79,7 @@ if __name__ == '__main__':
 
     except:
         import traceback
+
         traceback.print_exc()
 
         link.close()
