dd8939a1135b664b120a5f454a0eabf6c424c001
Alex Liu <contactalexliu@users.noreply.github.com>
2020-10-31 05:46:59 +0000

    Switch to dish and radio sockets

--- Telemetry/radio.py
+++ Telemetry/radio.py
@@ -7,10 +7,10 @@ import msgpack
 import platform
 import queue
 import serial
-import time
 import threading
-import zmq
 import traceback
+import zmq
+
 # Set logging verbosity.
 # CRITICAL will not log anything.
 # ERROR will only log exceptions.
@@ -18,10 +18,10 @@ import traceback
 log_level = logging.INFO
 
 # ZeroMQ Context.
-context = zmq.Context()
+context = zmq.Context.instance()
 # Define the socket using the Context.
-sock = context.socket(zmq.RADIO)
-sock.connect('udp://224.0.0.1:28650')
+radio = context.socket(zmq.RADIO)
+radio.connect('udp://224.0.0.1:28650')
 
 # Define message end sequence.
 end = b'EOM\n'
@@ -30,40 +30,47 @@ def readFromArduino(queue, exit_event):
     '''Read data from serial.'''
     while not exit_event.is_set():
         try:
-            #queue.put(link.read_until(end).rstrip(end))
-            queue.put(msgpack.packb("test string"))
+            queue.put(link.read_until(end).rstrip(end))
             logging.info('Producer received data.')
         except:
             traceback.print_exc()
             exit_event.set()
     logging.info('Producer received event. Exiting now.')
-    #link.close()
-    
+    link.close()
+
 def sendZmqMulticast(queue, exit_event):
     '''Multicast data with ZeroMQ.'''
     while not exit_event.is_set() or not queue.empty():
         try:
-            sock.send(queue.get())
+            # queue.get(True, 2) blocks with a 2 second timeout
+            # If still empty after 2 seconds, throws Queue.Empty
+            radio.send(queue.get(True, 2), group='telemetry')
             logging.info('Consumer sending data. Queue size is %d.', queue.qsize())
+        except Queue.Empty:
+            pass    # no message ready yet
         except:
             traceback.print_exc()
             exit_event.set()
     logging.info('Consumer received event. Exiting now.')
-    sock.close()
-    
+    radio.close()
+
 if __name__ == '__main__':
     try:
         logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=log_level, datefmt="%H:%M:%S")
-       # if platform.system() == 'Darwin':
-       #     link = serial.Serial('/dev/tty.usbmodem14101', 115200)
-       # elif platform.system() == 'Linux':
-       #     link = serial.Serial('/dev/ttyACM0', 115200)
-       # else:
-       #     link = serial.Serial('COM9', 115200)
+        # Open serial connection
+        if platform.system() == 'Darwin':
+            link = serial.Serial('/dev/tty.usbmodem14101', 500000)
+        elif platform.system() == 'Linux':
+            link = serial.Serial('/dev/ttyACM0', 500000)
+        else:
+            link = serial.Serial('COM9', 500000)
         # Throw away first reading
-       # _ = link.read_until(end).rstrip(end)
+        _ = link.read_until(end).rstrip(end)
+        # Set up data queue
         pipeline = queue.Queue(maxsize=100)
+        # Create exit event
         exit_event = threading.Event()
+        # Spawn worker threads
         with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
             executor.submit(readFromArduino, pipeline, exit_event)
             executor.submit(sendZmqMulticast, pipeline, exit_event)
